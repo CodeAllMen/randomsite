405c50fe3e490afceb803aebc1b259276fb78c93
yy <a1023767856@163.com>
2020-07-10 15:42:17 +0800

    initialize

---
+++ config_origin.py
@@ -0,0 +1,12 @@
+"""
+Create by yy on 2020/7/10
+"""
+
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = '8JBSO789E_#*@*)sjKNEI_@&*SBY'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Akira240611@localhost:3306/flaskblog?charset=utf8mb4'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
